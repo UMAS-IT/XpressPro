31b68f7a0132e5ebe3c4cbe3e9557ce13d537201
RojoSenpai <103008368+RojoSenpai@users.noreply.github.com>
2022-10-27 08:46:29 -0700

    part-3
    part-3

---
+++ Orion.Helper/Misc/GV.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Helper.Misc
+{
+    public static class GV
+    {
+        public static int RandomProjectNumber()
+        {
+            Random random = new Random();
+            return random.Next(0, 99999);
+        }
+        public static DateTime GetPacificStandardTime()
+        {
+            var utc = DateTime.UtcNow;
+            TimeZoneInfo pacificZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            var pacificTime = TimeZoneInfo.ConvertTimeFromUtc(utc, pacificZone);
+            return pacificTime;
+        }
+
+        public static string RandomName()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(RandomString(10, true));
+            builder.Append(RandomNumber(1000, 9999));
+            builder.Append(RandomString(10, false));
+            return builder.ToString();
+        }
+
+        private static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        private static string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+
+
+    }
+}
