982b707522205dca0a094b0cdd731af5be3de2ef
TheCraxyMan <103008368+RojoSenpai@users.noreply.github.com>
2023-03-13 02:09:37 -0700

    start hinerit catalog
    start hinerit catalog

--- Orion.Domain/ABB/Catalog/CatalogB2.cs
+++ Orion.Domain/ABB/Catalog/CatalogB2.cs
@@ -12,22 +12,8 @@ using System.Threading.Tasks;
 namespace Orion.Domain.EntityCatalogABB
 {
     // ABB - VCR
-    public class CatalogB2 : ValidatableBindableBase, ICatalog
+    public class CatalogB2 : Catalog
     {
-        private int _id;
-        public int Id
-        {
-            get => _id;
-            set => SetProperty(ref _id, value);
-        }
-
-        private string _model;
-        public string Model
-        {
-            get => _model;
-            set => SetProperty(ref _model, value);
-        }
-
         private double _Hp;
         public double Hp
         {
@@ -72,90 +58,18 @@ namespace Orion.Domain.EntityCatalogABB
 
 
 
-
-
-
-
-
-
-
-
-
-        private double _listPrice;
-        public double ListPrice
-        {
-            get => _listPrice;
-            set
-            {
-                SetProperty(ref _listPrice, value);
-                Cost = value * CostMultiplier;
-                SellPrice = Cost / (1 - (SellMargin / 100));
-            }
-        }
-
-        private double _costMultiplier;
-        public double CostMultiplier
-        {
-            get => _costMultiplier;
-            set
-            {
-                SetProperty(ref _costMultiplier, value);
-                Cost = value * ListPrice;
-                SellPrice = Cost / (1 - (SellMargin / 100));
-            }
-        }
-
-        private double _cost;
         [NotMapped]
-        public double Cost
-        {
-            get => (ListPrice * CostMultiplier).Truncate(2);
-            set => SetProperty(ref _cost, value);
-        }
-
-        private double _sellMargin;
-        public double SellMargin
-        {
-            get => _sellMargin;
-            set
-            {
-                SetProperty(ref _sellMargin, value);
-                SellPrice = Cost / (1 - (value / 100));
-            }
-        }
-
-        private double _SellPrice;
+        public override string Company { get => "ABB"; }
         [NotMapped]
-        public double SellPrice
-        {
-            get => (Cost / (1 - (SellMargin / 100))).Truncate(2);
-            set => SetProperty(ref _SellPrice, value);
-        }
-
-        private int? _dataSheetId;
-        public int? DataSheetId
-        {
-            get => _dataSheetId;
-            set => SetProperty(ref _dataSheetId, value);
-        }
-
-        private DataSheet _dataSheet;
-        public DataSheet DataSheet
-        {
-            get => _dataSheet;
-            set => SetProperty(ref _dataSheet, value);
-        }
-
+        public override string Product { get => "VCR"; }
         [NotMapped]
-        public string Company { get => "ABB"; }
-        [NotMapped]
-        public string Product { get => "VCR"; }
-        [NotMapped]
-        public string Index { get => "b2"; }
+        public override string Index { get => "b2"; }
 
-        public CatalogB2()
+        public CatalogB2() : base()
         {
             Description = "";
+            Nema = "";
+            Voltage = "";
         }
     }
 }
---
+++ Orion.Domain/Entity/Catalog.cs
@@ -0,0 +1,105 @@
+﻿using Orion.Binding.Binding;
+using Orion.Helper.Extension;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orion.Domain.Entity
+{
+    public class Catalog : ValidatableBindableBase, ICatalog
+    {
+        private int _id;
+        public int Id
+        {
+            get => _id;
+            set => SetProperty(ref _id, value);
+        }
+
+        private string _model;
+        public string Model
+        {
+            get => _model;
+            set => SetProperty(ref _model, value);
+        }
+
+        private double _listPrice;
+        public double ListPrice
+        {
+            get => _listPrice;
+            set
+            {
+                SetProperty(ref _listPrice, value);
+                Cost = value * CostMultiplier;
+                SellPrice = Cost / (1 - (SellMargin / 100));
+            }
+        }
+
+        private double _costMultiplier;
+        public double CostMultiplier
+        {
+            get => _costMultiplier;
+            set
+            {
+                SetProperty(ref _costMultiplier, value);
+                Cost = value * ListPrice;
+                SellPrice = Cost / (1 - (SellMargin / 100));
+            }
+        }
+
+        private double _cost;
+        [NotMapped]
+        public double Cost
+        {
+            get => (ListPrice * CostMultiplier).Truncate(2);
+            set => SetProperty(ref _cost, value);
+        }
+
+        private double _sellMargin;
+        public double SellMargin
+        {
+            get => _sellMargin;
+            set
+            {
+                SetProperty(ref _sellMargin, value);
+                SellPrice = Cost / (1 - (value / 100));
+            }
+        }
+
+        private double _SellPrice;
+        [NotMapped]
+        public double SellPrice
+        {
+            get => (Cost / (1 - (SellMargin / 100))).Truncate(2);
+            set => SetProperty(ref _SellPrice, value);
+        }
+
+        private int? _dataSheetId;
+        public int? DataSheetId
+        {
+            get => _dataSheetId;
+            set => SetProperty(ref _dataSheetId, value);
+        }
+
+        private DataSheet _dataSheet;
+        public DataSheet DataSheet
+        {
+            get => _dataSheet;
+            set => SetProperty(ref _dataSheet, value);
+        }
+
+        [NotMapped]
+        public virtual string Company { get => "Default Company Name"; }
+        [NotMapped]
+        public virtual string Product { get => "Default Product Name"; }
+        [NotMapped]
+        public  virtual string  Index { get => "Default Index Name"; }
+
+        public Catalog()
+        {
+            Model = "";
+        }
+    }
+}
